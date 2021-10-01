a25efd4721fd22bd8c29de6585b22619cedd8f52
Crystal <Crystal@DESKTOP-81QTH9S>
2021-09-30 21:11:34 -0400

    Updated

--- src/demo/GitDemo.java
+++ src/demo/GitDemo.java
@@ -4,6 +4,14 @@ public class GitDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World...");
+		
+		boolean isSunday = true;
+		
+		if(isSunday) {
+			System.out.println("abc");
+		}else {
+			System.out.println("Xyz");
+		}
 	}
 
 }
